95e13ad7bbbb793dd7e5fa3d27d958f5bb3db6ad
Simo Ahava <simo.s.ahava@gmail.com>
2017-01-22 22:15:42 +0200

    Reorganized examples and wdio configs

---
+++ examples/examples.conf.js
@@ -0,0 +1,32 @@
+const enhancedEcommerceSchema = require('../lib/enhancedEcommerceSchema.json')
+exports.config = {
+    specs: [
+        './examples/spec/examples.js'
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        maxInstances: 5,
+        browserName: 'phantomjs'
+    }],
+    sync: true,
+    logLevel: 'silent',
+    coloredLogs: true,
+    bail: 0,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    services: ['selenium-standalone'],
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd'
+    },
+    before: function() {
+        const chai = require('chai');
+        chai.use(require('chai-json-schema'))
+        chai.use(require('chai-subset'))
+        chai.tv4.addSchema('/enhancedEcommerceSchema.json', enhancedEcommerceSchema)
+        global.expect = chai.expect
+        global.assert = chai.assert
+    }
+}
