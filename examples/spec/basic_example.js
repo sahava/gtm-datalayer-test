d6162e6730b0675569b78366c87b4c3ff10ffe35
Simo Ahava <simo.s.ahava@gmail.com>
2017-01-23 19:49:32 +0200

    Fixed test spec to treat generic keys as, well, generic...

--- examples/spec/basic_example.js
+++ examples/spec/basic_example.js
@@ -1,58 +1,55 @@
+// Set the filename in require() to point to this test's configuration JSON
+const dataLayerConf = require('./basic_example.conf.json')
+
 const testUtils = require('../../lib/testUtils')
 const getWindowDataLayer = testUtils.getWindowDataLayer
 const getJsonSchema = testUtils.getJsonSchema
-const dataLayerConf = require('./basic_example.conf.json')
 const dataLayerName = dataLayerConf.dataLayerName || 'dataLayer'
 
 testUtils.validate(dataLayerConf)
 
 describe('Testing Google Tag Manager\'s dataLayer composition on ' + dataLayerConf.baseUrl, () => {
 
-    context('with generic tests', () => {
+    dataLayerConf.page.forEach(testPage => {
 
-        context('on page ' + dataLayerConf.basePath, () => {
+        context('navigating to page ' + testPage.path, () => {
 
-            before(() =>
-                browser.url(dataLayerConf.baseUrl + dataLayerConf.basePath).waitForVisible('body', 5000)
-            )
+            context('running generic dataLayer tests', () => {
 
-            it('expect dataLayer to be an Array', () =>
-                expect(getWindowDataLayer(dataLayerName)).to.be.an('Array')
-            )
+                before(() =>
+                    browser.url(dataLayerConf.baseUrl + testPage.path).waitForVisible('body', 5000)
+                )
 
-            it('expect dataLayer to have the gtm.js event', () => {
-                const objectsWithGtmJs = getWindowDataLayer(dataLayerName).filter(o => o.event === 'gtm.js')
-                if (!dataLayerConf.multipleContainers) {
-                    assert.lengthOf(objectsWithGtmJs, 1, "dataLayer should have exactly one gtm.js event")
-                } else {
-                    assert.isAtLeast(objectsWithGtmJs.length, 1, "dataLayer should have at least one gtm.js event")
-                }
-            })
+                it('expect dataLayer to be an Array', () =>
+                    expect(getWindowDataLayer(dataLayerName)).to.be.an('Array')
+                )
 
-            it('expect dataLayer to have globally defined keys', () => {
-                dataLayerConf.dataLayer.forEach(dlObj => {
-                    if (dlObj['@json'] === false) {
-                        delete dlObj['@json']
-                        assert.containSubset(getWindowDataLayer(dataLayerName), [dlObj], 'FAILED ON: ' + JSON.stringify(dlObj), null, 2)
+                it('expect dataLayer to have the gtm.js event', () => {
+                    const objectsWithGtmJs = getWindowDataLayer(dataLayerName).filter(o => o.event === 'gtm.js')
+                    if (!dataLayerConf.multipleContainers) {
+                        assert.lengthOf(objectsWithGtmJs, 1, "dataLayer should have exactly one gtm.js event")
                     } else {
-                        assert.jsonSchema(
-                            getWindowDataLayer(dataLayerName),
-                            getJsonSchema(dlObj),
-                            'FAILED ON: ' + JSON.stringify(dlObj, null, 2)
-                        )
+                        assert.isAtLeast(objectsWithGtmJs.length, 1, "dataLayer should have at least one gtm.js event")
                     }
                 })
+
+                it('expect dataLayer to have globally defined keys', () => {
+                    dataLayerConf.dataLayer.forEach(dlObj => {
+                        if (dlObj['@json'] === false) {
+                            delete dlObj['@json']
+                            assert.containSubset(getWindowDataLayer(dataLayerName), [dlObj], 'FAILED ON: ' + JSON.stringify(dlObj), null, 2)
+                        } else {
+                            assert.jsonSchema(
+                                getWindowDataLayer(dataLayerName),
+                                getJsonSchema(dlObj),
+                                'FAILED ON: ' + JSON.stringify(dlObj, null, 2)
+                            )
+                        }
+                    })
+                })
             })
-        })
 
-    })
-
-    context('with custom tests', () => {
-
-        dataLayerConf.page.forEach(testPage => {
-
-            context('on page ' + testPage.path, () => {
-
+            context('running page-specific tests', () => {
                 before(() =>
                     browser.url(dataLayerConf.baseUrl + testPage.path).waitForVisible('body', 5000)
                 )
